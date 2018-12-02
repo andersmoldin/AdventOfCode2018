b11ca0c598ad3f4a980c6609f91124633e052451
andersmoldin <anders@moldin.com>
2018-12-02 17:57:33 +0100

    Day 1

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _01
 {
@@ -6,7 +9,36 @@ namespace _01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadLines("input.txt");
+
+            Console.WriteLine($"Part 1: {Part1(input)}");
+            Console.WriteLine($"Part 2: {Part2(input)}");
+        }
+        
+        private static int Part1(IEnumerable <string> changes)
+        {
+            return changes.Select(f => int.Parse(f)).Sum();
+        }
+
+        private static int Part2(IEnumerable<string> changes)
+        {
+            var frequencyLog = new HashSet<int>();
+            var frequency = 0;
+
+            while (true)
+            {
+                foreach (var change in changes)
+                {
+                    frequency += Convert.ToInt32(change);
+
+                    if (frequencyLog.Contains(frequency))
+                    {
+                        return frequency;
+                    }
+
+                    frequencyLog.Add(frequency);
+                }
+            }
         }
     }
 }
