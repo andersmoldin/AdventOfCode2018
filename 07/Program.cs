4295440536e49d5d43c258451933af7f2449e3cd
andersmoldin <anders@moldin.com>
2018-12-04 19:09:07 +0100

    Prepared for coming days

---
+++ 07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").ToList();
+
+            Console.WriteLine($"Part 1: {Part1(input)}");
+            Console.WriteLine($"Part 2: {Part2(input)}");
+        }
+
+        private static int Part1(List<string> input)
+        {
+            return -1;
+        }
+
+        private static int Part2(List<string> input)
+        {
+            return -1;
+        }
+    }
+}
