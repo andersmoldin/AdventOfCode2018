60731a4eeebd2d15c73b05051b2bf19b382cb1cd
andersmoldin <anders@moldin.com>
2018-12-05 22:16:25 +0100

    Day 5 part 1

--- 05/Program.cs
+++ 05/Program.cs
@@ -9,18 +9,43 @@ namespace _05
     {
         static void Main(string[] args)
         {
-            var input = File.ReadLines("input.txt").ToList();
+            var input = File.ReadAllText("input.txt");
 
             Console.WriteLine($"Part 1: {Part1(input)}");
             Console.WriteLine($"Part 2: {Part2(input)}");
         }
 
-        private static int Part1(List<string> input)
+        private static int Part1(string input)
         {
-            return -1;
+            bool changedInput = false;
+            bool stillHeterozygots = true;
+
+            while (stillHeterozygots)
+            {
+                for (int i = 0; i < input.Length - 1; i++)
+                {
+                    if (Math.Abs(input[i] - input[i + 1]) == 32)
+                    {
+                        input = input.Substring(0, i) + input.Substring(i + 2);
+                        changedInput = true;
+                        break;
+                    }
+                }
+
+                if (!changedInput)
+                {
+                    stillHeterozygots = false;
+                }
+                else
+                {
+                    changedInput = false;
+                }
+            }
+
+            return input.Length;
         }
 
-        private static int Part2(List<string> input)
+        private static int Part2(string input)
         {
             return -1;
         }
